cc641c7ba51fdf6b22ea22adc4d577e3ecaa845f
Ana G. Mendes Pedroso <anagmendesp@gmail.com>
2019-04-14 16:14:34 -0400

    EscalonadorRR, EscalonadorFCFS e Despachante: a saga...

--- Code/EscalonadorRoundRobin.java
+++ Code/EscalonadorRoundRobin.java
@@ -1,13 +1,16 @@
 import java.util.Vector;
 
 public class EscalonadorRoundRobin extends Thread{
-private long timeQuantum;
-private static Vector<Processo> filaProntos = new Vector<Processo>();
+    private long timeQuantum;
+    private static Vector<Processo> filaProntos = new Vector<Processo>();
+    private Despachante despachante;
 
     public EscalonadorRoundRobin(long tq, Vector<Processo>filaProntos){
         this.timeQuantum = tq;
+        this.filaProntos = filaProntos;
     }
     public long getTimeQuantum() {
         return timeQuantum;
-    }    
+    }
+        
 }
