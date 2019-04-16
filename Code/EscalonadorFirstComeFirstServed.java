b69302162fbc329d697dfdb1e82c4ac44bcd714e
GabrielCansancao <gabrielkansan@gmail.com>
2019-04-16 16:57:24 -0400

    ListaProntos atualizada

--- Code/EscalonadorFirstComeFirstServed.java
+++ Code/EscalonadorFirstComeFirstServed.java
@@ -1,25 +1,25 @@
 import java.util.Vector;
 
 class EscalonadorFirstComeFirstServed implements Runnable {
-	private FilaEntrada listaEntrada;
+	private FilaEntradaCompartilhada filaEntrada;
 	private Memoria mem;
-	private ListaProntosCompartilhada listaProntos;
+	private FilaProntosCompartilhada filaProntos;
 
-	public EscalonadorFirstComeFirstServed(FilaEntrada listaEntrada,ListaProntosCompartilhada listaProntos, Memoria mem) {
-		this.listaEntrada = listaEntrada;
+	public EscalonadorFirstComeFirstServed(FilaEntradaCompartilhada filaEntrada,FilaProntosCompartilhada filaProntos, Memoria mem) {
+		this.filaEntrada = filaEntrada;
 		this.mem = mem;
-		this.listaProntos = listaProntos;
+		this.filaProntos = filaProntos;
 	}
 
 	@Override
 	public void run() {
 		//mem.wait();
-		if(!listaProntos.getFilaProntos().isEmpty()) {
-		Processo p = filaEntrada.removePrimeiro();
+		if(!filaProntos.getFilaProntos().isEmpty()) {
+		Processo p = filaEntrada.removeProcessoDaFilaDeEntrada();
 		
 		System.out.printf("Escalonador FCFS de longo prazo escolheu o processo id %d%n", p.getIdProcesso());
 		if (mem.getEspacoLivre() < p.getTamProcesso()) {
-			listaProntos.addListaProntos(p);
+			filaProntos.addListaProntos(p);
 			System.out.printf("Escalonador FCFS de longo prazo retirou o processo id %d da fila de entrada, colocando-o na fila de prontos%n",p.getIdProcesso());
 			this.notifyAll();
 		} else {
